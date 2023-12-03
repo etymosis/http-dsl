175339cf582eba74a58b75964127592d7fc83df2
Andrew Kennedy <alkennedy.academic@gmail.com>
2023-12-03 18:32:32 -0400

    init commit

---
+++ README.md
@@ -0,0 +1,5 @@
+#INTRO
+http is immensely powerful and a lot of internet traffic is done over http. Integrating services together is often done with REST, where developers need to spend valuble configuring an http client to speak a particular API's syntax and consume the payload in a service-specific way.
+
+The goal of this project is to simply http request/response for REST apis. Ideally to strip down http requests to a series of configuration files describing how to build the request and how to parse the response in a language-agnostic way.
+
---
+++ index.ts
@@ -0,0 +1,32 @@
+interface HttpDsl<T> {
+    execute(reqContext: RequestContext): ResponseContext<T>
+}
+
+interface RequestContext {
+    [key: string]: any;
+}
+
+interface ResponseContext<T> {
+    rawResponse: {
+        status: string;
+        headers: {
+            [key: string]: string
+        }
+        body: unknown;
+    }
+    parsedContext: T; 
+}
+
+class HttpDslError extends Error {
+
+}
+
+class MyClass {
+
+}
+
+const hd: HttpDsl<MyClass | Error> = {
+    execute: function (reqContext: RequestContext): ResponseContext<MyClass | Error> {
+        throw new Error("Function not implemented.");
+    }
+}
